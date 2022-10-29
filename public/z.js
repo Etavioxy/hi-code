9a7385abd8dba7b4224ffc67514f900b6e52d8bb
z7z-Eta <Etavioxy@foxmail.com>
2022-10-29 16:27:59 +0800

    init

--- public/z.js
+++ public/z.js
@@ -1,6 +1,38 @@
+var markdownit = window.markdownit()
+  .use(texmath, {engine: katex, delimiters: 'dollars'});
+
+var markdownitVue = (vue) => {
+  vue.component('markdownit',{
+    props:{
+      text:{type:String,default:''}
+    },
+    computed:{
+      html(){
+        return markdownit.render(this.text);
+      }
+    },
+    template:"<div v-html='html'></div>"
+  })
+};
 
 const app = Vue.createApp({
-  data: () => ({
-    code: code
-  })
-}).mount('#app')
+  data(){
+    return {
+      data: {
+        'hi-code':[
+          {name:'code', text:{code:''}}
+        ]
+      }
+    };
+  },
+  mounted(){
+    fetch('code')
+    .then((res)=>res.text())
+    .then((txt)=>{
+      this.data = JSON.parse(txt);
+    });
+  }
+})
+.use(hljsVuePlugin)
+.use(markdownitVue)
+.mount('#app')
