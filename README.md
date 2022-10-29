3d29ef8cb811f8a916913ac8ac9b60d2b42b63cb
z7z-Eta <Etavioxy@foxmail.com>
2022-10-29 16:27:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Hi Code
+
+打印ACM模板用。
+
+```shell
+node a.js [codedir]
+```
+
+然后使用浏览器打开localhost:[port]，使用浏览器的打印功能。
+
+---
+
+文件末尾带有/**/的注释，会转化为markdown文件
+
+使用.classify.json配置文件，可以实现分组。
+
+```json
+{
+	"Data Structure I":[
+		"stk",
+		"que",
+		"prior",
+		...
+	],
+	"Data Structure II":[
+	],
+	"Data Structure III":[
+	],
+	"String":[
+	],
+	...
+}
+```
--- a.js
+++ a.js
@@ -1,26 +1,62 @@
 const fs = require('fs')
 const url = require('url')
 const http = require('http')
+const process = require('process')
+const path = require('path')
 
+if( process.argv.length<=2 ){
+  console.log('usage: node a.js [codedir]')
+  process.exit(0x1);
+}
+let codeDir = process.argv[2];
+codeDir.replace('~', process.env.HOME)
+
+// from string to {code:'', comment:''}
+// the comment in /* */ will be extracted to {comment}
+function parseCode(s){
+  s = s.trimRight();
+  let match = Array.from(s.matchAll(/(\/\*)|(\*\/)/g));
+  if( match.length!=2 ) return {code:s};
+  let [l,r] = match.map(x=>x.index);
+  if( s.length-2!=r ) return {code:s};
+  return {
+    code: s.substr(0, l-1),
+    comment: s.substr(l+3, r-l-3)
+  };
+}
 function readCode(){
-  a = fs.readdirSync("assets/'").map(item => ({name:item, text:fs.readFileSync("assets/'/" + item).toString('utf-8')}))
-  return JSON.stringify(a)
+  let classifyFile = path.join(codeDir, '.classify.json');
+  let a = {};
+  try{
+    fs.accessSync(classifyFile, fs.constants.R_OK);
+    a = JSON.parse(fs.readFileSync(classifyFile));
+  }catch(err){
+    a = {'-*- code -*-': fs.readdirSync(codeDir)};
+  }
+  Object.keys(a).forEach(key => {
+    a[key] = a[key].map(item => ({
+      name: item,
+      text: parseCode(fs.readFileSync(path.join(codeDir,item)).toString('utf-8'))
+    }));
+  });
+  return JSON.stringify(a);
 }
 
-const server = http.createServer((req, res) => {
-  console.log('connect')
-  let pathname = url.parse(req.url).pathname
+console.log(readCode());
 
-  console.log('pathname: ', pathname)
+const server = http.createServer((req, res) => {
+  let pathname = url.parse(req.url).pathname
+  console.log('connect - pathname: ', pathname)
 
   if( pathname === '/code' ){
     res.end(readCode())
     return ;
   }
 
-  if( pathname.indexOf('.') === -1 ) pathname += 'index.html';
+  if( pathname === '/' ) pathname += 'index.html';
 
-  fs.readFile('src'+pathname, (err, data) => {
+  pathname = 'public'+pathname;
+  fs.readFile(pathname, (err, data) => {
     if(err){
       console.log(err.message)
       res.end('404')
